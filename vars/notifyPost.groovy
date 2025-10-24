a781d3b5fd1a0119852f5b08c8350bfda8455754
AMAL DEEP <161101203+Amaldeep98@users.noreply.github.com>
2025-10-24 23:23:30 +0530

    Update notifyPost.groovy

--- vars/notifyPost.groovy
+++ vars/notifyPost.groovy
@@ -1,20 +1,19 @@
+// jenkins-shared-library/vars/notifyPost.groovy
 def call() {
-    post {
-        success {
-            echo "✅ Pipeline Success"
-            echo "Job: ${env.JOB_NAME}"
-            echo "Build Number: ${env.BUILD_NUMBER}"
-            echo "URL: ${env.BUILD_URL}"
-        }
-        failure {
-            echo "❌ Pipeline Failed"
-            echo "Job: ${env.JOB_NAME}"
-            echo "URL: ${env.BUILD_URL}"
-        }
-        aborted {
-            echo "🚫 Pipeline Aborted"
-            echo "Job: ${env.JOB_NAME}"
-            echo "URL: ${env.BUILD_URL}"
-        }
+    if (currentBuild.currentResult == 'SUCCESS') {
+        echo "✅ Pipeline Success"
+        echo "Job: ${env.JOB_NAME}"
+        echo "Build Number: ${env.BUILD_NUMBER}"
+        echo "URL: ${env.BUILD_URL}"
+    } else if (currentBuild.currentResult == 'FAILURE') {
+        echo "❌ Pipeline Failed"
+        echo "Job: ${env.JOB_NAME}"
+        echo "URL: ${env.BUILD_URL}"
+    } else if (currentBuild.currentResult == 'ABORTED') {
+        echo "🚫 Pipeline Aborted"
+        echo "Job: ${env.JOB_NAME}"
+        echo "URL: ${env.BUILD_URL}"
+    } else {
+        echo "ℹ️ Pipeline finished with status: ${currentBuild.currentResult}"
     }
 }
